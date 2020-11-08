8a53dc9f8d61d49b18ed08a424c09f7db7c3f786
Thomas Frei <7283497+thfrei@users.noreply.github.com>
2020-11-08 17:18:10 +0100

    use source-map for debugging

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   devServer: {
     contentBase: './dist',
     open: false,
