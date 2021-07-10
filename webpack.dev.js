217054486b9cd6afc7269a9e9c21c32fa907bb78
thfrei <7283497+thfrei@users.noreply.github.com>
2021-07-10 20:40:23 +0000

    working gitpod hot reloading

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,13 @@
 // const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 
+// running on gitpod - need to tell webpack dev-server to have the correct url
+let publicUrl;
+if (process.env.GITPOD_WORKSPACE_URL) {
+  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://')
+  const publicUrl = `8080-${host}`
+}
+
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -10,5 +17,6 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './dist',
     open: false,
+    public: publicUrl,
   },
 });
