4a64c117cb2e27e895b7205d246137e3cb217156
Thomas Frei <7283497+thfrei@users.noreply.github.com>
2021-07-30 23:49:11 +0200

    basic canvas editor divs

--- src/demo.ts
+++ src/demo.ts
@@ -5,10 +5,14 @@ const PKG_VERSION = process.env.PKG_VERSION
 // just skip one cycle, so we can make sure, all other libraries are loaded!
 setTimeout(() => {
   const idc = new InfiniteDrawingCanvas(
-    document.getElementById("c") as HTMLCanvasElement,
-    document.getElementById("parentContainer"),
-    document.getElementById("canvasContainer"),
+    document.getElementById("idc"),
   );
+
+  idc.setHeightCanvas(700);
+  idc.setWidthCanvas(1500);
+
+  idc.setHeightEditor(400);
+  idc.setWidthEditor(800);
 
   console.log("hi");
 
@@ -16,4 +20,6 @@ setTimeout(() => {
   document.getElementById("pkg-version").innerHTML = `v${PKG_VERSION}`;
 
   // add access to window, for debugging purposes.
+  // @ts-ignore
+  window.idc = idc;
 });
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,48 @@ import { fabric } from '../dist/assets/fabric';
 import 'hammerjs';
 
 class InfiniteDrawingCanvas {
-  canvas: fabric.Canvas;
+  fabric: fabric.Canvas;
+  canvasElement: HTMLCanvasElement;
   CanvasContainer: HTMLElement;
   EditorContainer: HTMLElement;
 
-  constructor(canvas: HTMLCanvasElement, CanvasContainer: HTMLElement, EditorContainer: HTMLElement) {
-    this.canvas = new fabric.Canvas(canvas, {
+  constructor(initialDiv: HTMLElement) {
+    this.EditorContainer = initialDiv;
+    this.canvasElement = document.createElement('canvas');
+    this.CanvasContainer = document.createElement('div');
+    this.CanvasContainer.appendChild(this.canvasElement);
+    this.EditorContainer.appendChild(this.CanvasContainer);
+
+    // add scrollbars if inner canvas gets bigger (infinite inner canvas)
+    this.EditorContainer.style.overflow = 'auto';
+    this.EditorContainer.style.resize = 'both';
+
+    this.fabric = new fabric.Canvas(this.canvasElement, {
       isDrawingMode: true,
+      selection: false,
     });
+
     return this;
+  }
+
+  setHeightEditor(height: number) {
+    this.EditorContainer.style.height = `${height}px`;
+  }
+
+  setWidthEditor(width: number) {
+    this.EditorContainer.style.width = `${width}px`;
+  }
+
+  setWidthCanvas(width: number) {
+    this.CanvasContainer.style.width = `${width}px`;
+    this.canvasElement.width = width;
+    this.fabric.setWidth(width);
+  }
+
+  setHeightCanvas(height: number) {
+    this.CanvasContainer.style.height = `${height}px`;
+    this.canvasElement.height = height;
+    this.fabric.setHeight(height);
   }
 }
 
